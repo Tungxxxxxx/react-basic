afa22e88b802af9b99a77dc837744b58e4a35050
TUNGPT <tungphamthanhcdt@gmail.com>
2023-09-17 23:17:00 +0700

    bài 9 - Form reactJs

--- src/views/App.js
+++ src/views/App.js
@@ -12,12 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          aaa <code>Phạm Thanh Tùng</code> Lớp 10000000000000000000.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          ahhahahahah
-        </a>
+
         <MyComponent />
       </header>
       <div></div>
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -3,51 +3,49 @@ import React from 'react';
 
 class MyComponent extends React.Component {
   state = {
-    name: 'Con me li ti',
-    age: 1000,
+    lastName: 'aaa',
+    firstName: '',
   };
-  handleOnchangeName = (event) => {
+  handleOnchangeLastName = (event) => {
     this.setState({
-      name: event.target.value,
+      lastName: event.target.value,
     });
   };
+  handleOnchangeFirstName = (event) => {
+    this.setState({
+      firstName: event.target.value,
+    });
+  };
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log('>>>Check data input:', this.state);
+    alert('Click me');
+  };
   render() {
-    let info = [
-      { id: 1, name: 'Tùng', age: 30 },
-      { id: 2, name: 'Hà', age: 16 },
-      { id: 3, name: 'Su', age: 2 },
-    ];
     return (
       <>
-        <p>Đây là sử dụng state</p>
-        <input type="text" value={this.state.name} onChange={(event) => this.handleOnchangeName(event)} />
-        <p>My name is {this.state.name}</p>
-        <table className="abc">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Age</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{info[0].id}</td>
-              <td>{info[0].name}</td>
-              <td>{info[0].age}</td>
-            </tr>
-            <tr>
-              <td>{info[1].id}</td>
-              <td>{info[1].name}</td>
-              <td>{info[1].age}</td>
-            </tr>
-            <tr>
-              <td>{info[2].id}</td>
-              <td>{info[2].name}</td>
-              <td>{info[2].age}</td>
-            </tr>
-          </tbody>
-        </table>
+        <label htmlFor="lName">Lastname</label>
+        <br />
+        <input
+          id="lName"
+          type="text"
+          value={this.state.lastName}
+          onChange={(event) => this.handleOnchangeLastName(event)}
+        />
+        <br />
+        <label htmlFor="fName">Firstname</label>
+        <br />
+        <input
+          type="text"
+          id="fName"
+          value={this.state.firstName}
+          onChange={(event) => {
+            this.handleOnchangeFirstName(event);
+          }}
+        />
+        <br />
+        <br />
+        <input type="submit" value="Submit" onClick={(event) => this.handleOnSubmit(event)} />
       </>
     );
   }
